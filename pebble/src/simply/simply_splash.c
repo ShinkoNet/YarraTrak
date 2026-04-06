d2e448511ec561c6ef2d2f68dfc9c25505246e4e
ShinkoNet <10773386+ShinkoNet@users.noreply.github.com>
2026-04-06 13:18:24 +0000

    theming

--- pebble/src/simply/simply_splash.c
+++ pebble/src/simply/simply_splash.c
@@ -3,29 +3,45 @@
 #include "simply.h"
 
 #include "util/graphics.h"
+#include "util/platform.h"
 
 #include <pebble.h>
 
-void layer_update_callback(Layer *layer, GContext *ctx) {
-  SimplySplash *self = (SimplySplash*) window_get_user_data((Window*) layer);
-
-  GRect frame = layer_get_frame(layer);
-
-#if defined(SPLASH_LOGO)
-  graphics_draw_bitmap_centered(ctx, self->image, frame);
+static GColor prv_splash_background_color(void) {
+#if defined(PBL_COLOR)
+  return GColorFromRGB(41, 19, 129);
 #else
-  graphics_draw_bitmap_in_rect(ctx, self->image, frame);
+  return GColorBlack;
+#endif
+}
+
+void layer_update_callback(Layer *layer, GContext *ctx) {
+  GRect frame = layer_get_frame(layer);
+  GRect logo_frame = frame;
+  logo_frame.origin.y -= 15;
+
+  graphics_context_set_fill_color(ctx, prv_splash_background_color());
+  graphics_fill_rect(ctx, frame, 0, GCornerNone);
+
+#if defined(PBL_PLATFORM_APLITE)
+  graphics_context_set_text_color(ctx, GColorWhite);
+  graphics_draw_text(ctx, "PTV Notify",
+                     fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD),
+                     GRect(0, 58, frame.size.w, 32),
+                     GTextOverflowModeTrailingEllipsis,
+                     GTextAlignmentCenter,
+                     NULL);
+#else
+  SimplySplash *self = (SimplySplash*) window_get_user_data((Window*) layer);
+  graphics_draw_bitmap_centered(ctx, self->image, logo_frame);
 #endif
 }
 
 
 static void window_load(Window *window) {
+#if !defined(PBL_PLATFORM_APLITE)
   SimplySplash *self = window_get_user_data(window);
-
-#if defined(SPLASH_LOGO)
   self->image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_LOGO_SPLASH);
-#else
-  self->image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TILE_SPLASH);
 #endif
 }
 
@@ -42,8 +58,8 @@ SimplySplash *simply_splash_create(Simply *simply) {
 
   self->window = window_create();
   window_set_user_data(self->window, self);
-  window_set_fullscreen(self->window, false);
-  window_set_background_color(self->window, GColorBlack);
+  window_set_fullscreen(self->window, true);
+  window_set_background_color(self->window, prv_splash_background_color());
   window_set_window_handlers(self->window, (WindowHandlers) {
     .load = window_load,
     .disappear = window_disappear,
@@ -63,4 +79,3 @@ void simply_splash_destroy(SimplySplash *self) {
 
   free(self);
 }
-
